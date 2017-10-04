2edb8e729365d36ac6b014113782c8fbbbb6153a
ChenESia <cheneus@gmail.com>
2017-10-04 13:50:40 -0500

    updated crystal game to update score and reintialize after win/lose

--- crystalGame/game.js
+++ crystalGame/game.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
   console.log("ready!")
-  var currentScore = targetScore = red = yellow = blue = green = win = lose = 0;
+  var currentScore = 0,
+    red, yellow, blue, green = 0;
+  var win = 0,
+    lose = 0;
   var redflag = yellowflag = blueflag = greenflag = false;
   // var crystalsRed = gameLogic.random(1, 12);
-  var targetScore = gameLogic.random(12, 100);
-  console.log(targetScore);
 
   function checkWin() {
-    if (targetScore == currentScore) {
+    if (gameLogic.targetScore === currentScore) {
       win++;
+      $('#winScore').html(win);
       alert("win");
+      gameLogic.initailize();
+      gameLogic.targetRandom();
     }
-    if (targetScore < currentScore) {
+    if (gameLogic.targetScore < currentScore) {
       lose++;
+      $('#loseScore').html(lose);
       alert("lose");
+      gameLogic.initailize();
+      gameLogic.targetRandom();
     }
   };
 
@@ -23,7 +30,6 @@ $(document).ready(function() {
       redflag = !redflag;
       console.log(red);
     }
-
     currentScore += red
     console.log(currentScore);
     $('#yourScore').text(currentScore);
@@ -63,20 +69,32 @@ $(document).ready(function() {
     checkWin();
   });
 
+  var gameLogic = {
+    targetScore: 0,
+    random: function(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    },
+
+    targetRandom: function() {
+      this.targetScore = gameLogic.random(12, 100),
+        $('#targetScore').html(this.targetScore);
+    },
+    initailize: function() {
+      currentScore = 0;
+      console.log("currentScore " + currentScore)
+      $('#yourScore').html(currentScore);
+      console.log(targetScore);
+      greenflag = redflag = yellowflag = blueflag = false;
 
 
+    },
+  };
+  gameLogic.targetRandom();
 });
 
-var gameLogic = {
-  random: function(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-  },
-  initailize: function() {
 
-  },
-};
 // var crystals = {
 //   red: {
 //     value: 0,
