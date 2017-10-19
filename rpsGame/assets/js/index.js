89beaa92af26849c93c14c7021d8c067a78ae298
ChenESia <cheneus@gmail.com>
2017-10-19 16:35:26 -0500

    added sign Anonymously

--- rpsGame/assets/js/index.js
+++ rpsGame/assets/js/index.js
@@ -34,14 +34,53 @@
  // Begin listening for data
  startListening();
 
- firebase.auth().signInAnonymously().catch(function(error) {
-   // Handle Errors here.
-   var errorCode = error.code;
-   var errorMessage = error.message;
-   // ...
+ // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+ //   // Handle Errors here.
+ //   var errorCode = error.code;
+ //   var errorMessage = error.message;
+ //   // ...
+ // });
+
+ // firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+ //   // Handle Errors here.
+ //   var errorCode = error.code;
+ //   var errorMessage = error.message;
+ //   // ...
+ // });
+
+ // firebase.auth().onAuthStateChanged(function(user) {
+ //   if (user) {
+ //     // User is signed in.
+ //     var displayName = user.displayName;
+ //     var email = user.email;
+ //     var emailVerified = user.emailVerified;
+ //     var photoURL = user.photoURL;
+ //     var isAnonymous = user.isAnonymous;
+ //     var uid = user.uid;
+ //     var providerData = user.providerData;
+ //     // ...
+ //   } else {
+ //     // User is signed out.
+ //     // ...
+ //   }
+ // });
+ // addEventListner , run signIn
+ $('#loginBtn').on('click', function() {
+   firebase.auth().signInAnonymously().catch(function(error) {
+     // Handle Errors here.
+     var errorCode = error.code;
+     var errorMessage = error.message;
+     console.log(error.message);
+     // ...
+   });
+ });
+
+ $('#loginOutBtn').on('click', function() {
+   firebase.auth().signOut();
  });
 
  firebase.auth().onAuthStateChanged(function(user) {
+   console.log(user);
    if (user) {
      // User is signed in.
      var isAnonymous = user.isAnonymous;
